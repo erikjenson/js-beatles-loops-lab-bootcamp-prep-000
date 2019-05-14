82e9eb6180a1f3268f059de29d5522d96bfea6a9
Erik Jenson <erik.a.jenson@gmail.com>
2019-05-14 13:50:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+var newArray = [];
+
+function theBeatlesPlay(mus,inst){
+  for (var i=0; i<4; i++){
+  newArray.push(`${mus[i]} plays ${inst[i]}`);
+  }
+  return newArray;
+}
+
+function addExclamation(list){
+  facts[i] = `${facts[i]} !!!`;
+}
+
+function johnLennonFacts(facts){
+  while(var i=) {
+
+  }
+  return facts
+}
